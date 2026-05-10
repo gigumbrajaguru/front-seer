f4f6ad288fd5f48953c31ced4cd495a23a1a1346
Gigum Bandara Rajaguru <gigumbrajaguru@gmail.com>
2026-05-10 15:15:51 +0530

    updated comments 0.7

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000',
+  apiBaseUrl: 'https://host.myseer.xyz',
   googleClientId: '574601845872-69q3ag715can9feav6liuoh7uhllhnn3.apps.googleusercontent.com',
   facebookClientId: '',
   tiktokClientKey: '',
-  discordClientId: ''
+  discordClientId: '',
 };
